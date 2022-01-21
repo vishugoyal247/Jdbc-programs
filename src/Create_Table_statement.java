b8acc6083e1e654ed9064b2c23ef08c368336b1c
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-21 20:46:05 +0530

    Statement_table Statement

---
+++ src/Create_Table_statement.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class Create_Table_statement
+{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
+        Statement st = con.createStatement();
+        st.executeUpdate("CREATE TABLE emp1 (name varchar(20),gender varchar(8),age int,salary decimal (5,3) );");
+//        st.close();
+        con.close();
+    }
+}
