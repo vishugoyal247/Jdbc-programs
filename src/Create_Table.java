ce0ebf169dc0cbc7627645dbb50ae4f5853b33b2
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-21 20:34:49 +0530

    Prepared Statement

--- src/Create_Table.java
+++ src/Create_Table.java
@@ -5,6 +5,7 @@ public class Create_Table
     public static void main(String[] args) throws ClassNotFoundException, SQLException
     {
 //        Class.forName("com.mysql.cj.jdbc.Driver");
+
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
         PreparedStatement ps = con.prepareStatement("CREATE TABLE emp (name varchar(20),gender varchar(8),age int,salary decimal (5,3) );");
         ps.executeUpdate();
