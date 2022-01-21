5e69b2275f9c8111d2cca28412376d742913aff3
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-21 19:10:10 +0530

    code added

--- src/Create_Table.java
+++ src/Create_Table.java
@@ -2,7 +2,8 @@ import java.sql.*;
 
 public class Create_Table
 {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
         PreparedStatement ps = con.prepareStatement("CREATE TABLE emp (name varchar(20),gender varchar(8),age int,salary decimal (5,3) );");
---
+++ src/Insert_data.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Insert_data
+{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+        Scanner sc = new Scanner(System.in);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
+        PreparedStatement ps = con.prepareStatement("INSERT INTO emp VALUES (?,?,?,? );");
+        System.out.print("Enter the name of the employee: ");
+        String name = sc.next();
+        System.out.print("Enter the gender male/female: ");
+        String gender = sc.next();
+        System.out.print("Enter the age of the employee: ");
+        int age = sc.nextInt();
+        System.out.print("Enter the salary of the employee: ");
+        float sal = sc.nextFloat();
+        ps.setString(1,name);
+        ps.setString(2,gender);
+        ps.setInt(3,age);
+        ps.setFloat(4,sal);
+        ps.executeUpdate();
+        ps.close();
+        con.close();
+    }
+}
