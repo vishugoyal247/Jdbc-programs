fa9dc3cfbbcbfe489cd79b9801a09e2c48f5c876
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-22 22:34:22 +0530

    order query

--- src/_013_Select_row_sorted_order.java
+++ src/_013_Select_row_sorted_order.java
@@ -6,7 +6,8 @@ public class _013_Select_row_sorted_order
     {
         Connection con  = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM emp1 ORDER BY salary");
+        ResultSet rs = st.executeQuery("SELECT * FROM emp1 ORDER BY salary ");
+        System.out.println("NAME         GENDER        AGE       SALARY");
         while (rs.next())
         {
             System.out.println(rs.getString(1)+"        "+rs.getString(2)+"        "+rs.getInt(3)+"        "+rs.getFloat(4));
