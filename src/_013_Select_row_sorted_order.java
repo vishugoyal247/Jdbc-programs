525187370410d476787ad10bbec9d02b85e9c701
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-22 22:10:36 +0530

    order query

---
+++ src/_013_Select_row_sorted_order.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class _013_Select_row_sorted_order
+{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+        Connection con  = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM emp1 ORDER BY salary");
+        while (rs.next())
+        {
+            System.out.println(rs.getString(1)+"        "+rs.getString(2)+"        "+rs.getInt(3)+"        "+rs.getFloat(4));
+        }
+        con.close();
+    }
+}
