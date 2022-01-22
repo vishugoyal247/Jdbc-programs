ce3ef7329d3050f5661e9aa955f89604d90448af
Vishu Goyal <goalsachiever8@gmail.com>
2022-01-22 20:57:28 +0530

    drop table by statement

---
+++ src/_004_Drop_Table.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public interface _004_Drop_Table
+{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
+        Statement st = con.createStatement();
+        st.executeUpdate("DROP TABLE emp");
+        con.close();
+
+    }
+}
