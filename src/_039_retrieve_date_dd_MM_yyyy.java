163bf6c314a10dac09da73217d47caddfcf32fa1
goalsachiever <goalsachiever8@gmail.com>
2022-01-24 23:43:16 +0530

    retrieve date in dd-MM-yyyy

---
+++ src/_039_retrieve_date_dd_MM_yyyy.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.sql.*;
+import java.text.CompactNumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class _039_retrieve_date_dd_MM_yyyy
+{
+    public static void main(String[] args) throws SQLException
+    {
+        Scanner sc = new Scanner(System.in);
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp","root","");
+        PreparedStatement ps = con.prepareStatement("select * from detail");
+        ResultSet rs = ps.executeQuery();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+        while (rs.next())
+        {
+            System.out.println("The details are : ");
+            System.out.println("The name of the employee : "+rs.getString(1));
+
+            java.sql.Date sdate = rs.getDate(2);
+            String dob = sdf.format(sdate);
+            System.out.println("The Date of birth is : "+dob );
+
+            System.out.println("The age of the employee : "+rs.getInt(3));
+            System.out.println("\n\n");
+        }
+        con.close();
+    }
+}
+
