5120a8b5e4ddae182ed0aca74b644f72b32ff318
goalsachiever <goalsachiever8@gmail.com>
2022-01-25 22:23:47 +0530

    Character type data retrieve from the database

---
+++ src/_043_Retrieve_CLOB_data.java
@@ -0,0 +1,38 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.*;
+
+public class _043_Retrieve_CLOB_data
+{
+    public static void main(String[] args) throws SQLException, IOException, SQLException {
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/people","root","");
+        PreparedStatement ps=con.prepareStatement("select * from cities");
+        ResultSet rs=ps.executeQuery();
+        int n=1;
+        while(rs.next())
+        {
+            FileWriter fw=new FileWriter("file"+n+".txt");
+            String name=rs.getString(1);
+            System.out.println(name);
+            Reader r=rs.getCharacterStream(2);
+            /*
+            char[] buf=new char[1024];
+            while(r.read(buf)>0)
+            {
+                fw.write(buf);
+            }
+             */
+            int i=r.read();
+            while(i!=-1)
+            {
+                fw.write(i);
+                i=r.read();
+            }
+            fw.flush();
+            System.out.println("Successfully retrieved file: file"+n+".txt");
+            n++;
+        }
+        con.close();
+    }
+}
