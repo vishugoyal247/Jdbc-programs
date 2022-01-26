0bf7ae4cccff68cad3d781966b1d9efce629a397
goalsachiever <goalsachiever8@gmail.com>
2022-01-27 00:21:54 +0530

    Database meta data

---
+++ src/_052_DatabaseMetaData2.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class _052_DatabaseMetaData2
+{
+    /*
+    DatabaseMetaData
+     */
+    public static void main(String[] args) throws SQLException
+    {
+        int count=0;
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcpractical?user=root&password=");
+        DatabaseMetaData dbmd=con.getMetaData();
+        String catalog=null;
+        String schemaPattern=null;
+        String tableNamePattern=null;
+        String[] types=null;
+        ResultSet rs=dbmd.getTables(catalog,schemaPattern,tableNamePattern,types);
+        while(rs.next())
+        {
+            count++;
+            System.out.println(rs.getString(3));
+        }
+
+        System.out.println("Number Of Tables :"+count);
+        con.close();
+    }
+}
